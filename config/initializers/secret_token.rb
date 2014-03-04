fd2d469aa42d777488c92982237a085b729f8a2d
Toan Nguyen <toannm3110@gmail.com>
2014-03-04 22:50:08 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DepvdServer::Application.config.secret_key_base = 'e24f16b9ccb9da98e8392dd7c46f52cb86d8f5466db1c09f69b8ff8fbf2f3b79917589a293e94cd483390d9f1c04ecff4af8ff8d2ed49c5895da44774fde3971'
